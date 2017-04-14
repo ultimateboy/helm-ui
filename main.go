0205d6c70889cb0eb13c0d46e85a4e2fce33638d
Matt Tucker <mtucker@deis.com>
2017-04-14 14:46:21 -0600

    actually serve a website instead of just log

--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 
 	"k8s.io/helm/pkg/helm"
 )
 
-func main() {
-	client := helm.NewClient(helm.Host(os.Getenv("TILLER_HOST")))
-	client.Option()
-	releases, err := client.ListReleases()
-	if err != nil {
-		log.Fatalf("failed to get helm client: %v", err)
-	}
-	log.Println("RELEASES:")
-	for _, r := range releases.GetReleases() {
-		log.Println(r.Name)
-	}
+type HelmClient struct {
+	c *helm.Client
+}
 
-	select {}
+func NewHelmClient(host string) *HelmClient {
+	return &HelmClient{
+		c: helm.NewClient(helm.Host(host)),
+	}
+}
+
+func (c HelmClient) listReleases(w http.ResponseWriter, r *http.Request) {
+	releases, err := c.c.ListReleases()
+	if err != nil {
+		log.Printf("failed to list releases: %v", err)
+		return
+	}
+	for _, r := range releases.GetReleases() {
+		io.WriteString(w, r.Name)
+	}
+}
+
+func main() {
+	client := NewHelmClient(os.Getenv("TILLER_HOST"))
+	http.HandleFunc("/", client.listReleases)
+
+	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
 }
