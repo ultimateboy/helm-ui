5d18be0920e280525b7e81a55e1f0c700c4dedb7
Matt Tucker <mtucker@deis.com>
2017-04-14 19:06:19 -0600

    add extra release details

--- main.go
+++ main.go
@@ -26,14 +26,24 @@ func (c HelmClient) listReleases(w http.ResponseWriter, r *http.Request) {
 		log.Printf("failed to list releases: %v", err)
 		return
 	}
-	for _, r := range releases.GetReleases() {
-		io.WriteString(w, r.Name)
+	if releases.Count == 0 {
+		return
 	}
+	io.WriteString(w, "<h1>Releases</h1>")
+	io.WriteString(w, "<table><tr><th>Namespace</th><th>Name</th><th>Version</th></tr>")
+	for _, r := range releases.GetReleases() {
+		io.WriteString(w, "<tr>")
+		io.WriteString(w, "<td>"+r.Namespace+"</td>")
+		io.WriteString(w, "<td>"+r.Name+"</td>")
+		io.WriteString(w, fmt.Sprintf("%s%d%s", "<td>", r.Version, "</td>"))
+		io.WriteString(w, "</tr>")
+	}
+	io.WriteString(w, "</table>")
 }
 
 func main() {
 	client := NewHelmClient(os.Getenv("TILLER_HOST"))
-	http.HandleFunc("/", client.listReleases)
 
+	http.HandleFunc("/", client.listReleases)
 	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
 }
