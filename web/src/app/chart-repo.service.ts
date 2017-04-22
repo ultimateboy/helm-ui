585bc75d49c45b4f4ae7956aaf6af6848e56683b
Erin Corson <frodopwns@gmail.com>
2017-04-22 14:28:00 -0600

    adding initial frontend code

---
+++ web/src/app/chart-repo.service.ts
@@ -0,0 +1,43 @@
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { ChartRepo } from './chart-repo';
+
+@Injectable()
+export class ChartRepoService {
+  private reposUrl = 'http://104.197.249.14/repos';  // URL to web api
+
+  constructor(private http: Http) { }
+
+  getRepos(): Promise<ChartRepo[]> {
+    return this.http.get(this.reposUrl)
+               .toPromise()
+               .then(response => response.json() as ChartRepo[])
+               .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  getReposSlowly(): Promise<ChartRepo[]> {
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getRepos()), 2000);
+    });
+  }
+
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  create(name: string, url: string): Promise<ChartRepo> {
+    return this.http
+      .post(this.reposUrl, JSON.stringify({name: name, url: url}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json() as ChartRepo)
+      .catch(this.handleError);
+  }
+
+}
