bca080c0af7bee1c52d533d9771421f8879ee473
Erin Corson <frodopwns@gmail.com>
2017-05-03 16:26:07 -0600

    initial material design implementation

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -3,12 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <nav>
-      <a routerLink="/chart-repos" routerLinkActive="active">Chart Repos</a>
-      <a routerLink="/releases" routerLinkActive="active">Releases</a>
-    </nav>
+  <md-sidenav-container [class.m2app-dark]="isDarkTheme">
+
+  <md-sidenav #sidenav mode="over" class="app-sidenav" color="primary">
+    <md-toolbar>
+      <span>Options</span>
+      <md-toolbar-row>
+        <a routerLink="/chart-repos" (click)="sidenav.toggle()" routerLinkActive="active"><button class="app-button" md-button>Chart Repos</button></a>
+      </md-toolbar-row>
+      <md-toolbar-row>
+        <a routerLink="/releases" (click)="sidenav.toggle()" routerLinkActive="active"><button class="app-button" md-button>Releases</button></a>
+      </md-toolbar-row>
+    </md-toolbar>
+   </md-sidenav>
+
+  <md-toolbar color="primary">
+    <button md-button class="app-menu-button" (click)="sidenav.toggle()">
+      <i class="material-icons">menu</i>
+    </button>
+    <span class="title">{{title}}</span>
+  </md-toolbar>
     <router-outlet></router-outlet>
+    </md-sidenav-container>
   `,
   styleUrls: ['./app.component.css'],
 })
